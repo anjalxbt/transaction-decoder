b1300620deb32cfab9d7bccdf1b4d8ca98a93366
sudosuanjal <anjalehackchey101@gmail.com>
2026-01-03 22:37:24 +0530

    feat: version seperator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #[allow(unused_variables)]
 fn read_version(transaction_hex:&str)->u32{
+    let transaction_bytes = hex::decode(transaction_hex).unwrap();
+    let version_bytes = &transaction_bytes[0..4];
+    println!("version bytes: {:?}", version_bytes);
     return 1;
 }
 
 fn main() {
-    let version = read_version("adhfaweorhawer");
+    let version = read_version("0123456789");
     println!("version: {}!", version);
 }
