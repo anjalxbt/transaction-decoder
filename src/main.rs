431a337fd01c519ea62ec1731bd714b895be84d2
anjalxbt <anjalehackchey101@gmail.com>
2026-01-13 03:20:21 +0530

    feat: added serde serialize

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
+use serde::Serialize;
 use std::io::Read;
 
-struct Input{
-    txid:[u8; 32],
-    output_index:u32,
-    script:Vec<u8>,
-    sequence:u32
+#[derive(Debug, Serialize)]
+struct Input {
+    txid: [u8; 32],
+    output_index: u32,
+    script: Vec<u8>,
+    sequence: u32,
 }
 
-fn read_compact_size(transaction_bytes: &mut &[u8])->u64{
-    let mut compact_size = [0_u8;1];
+fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
+    let mut compact_size = [0_u8; 1];
     transaction_bytes.read(&mut compact_size).unwrap();
 
-    match compact_size[0]{
-        0..=252=> compact_size[0] as u64,
-        253=>{
-            let mut buffer = [0;2];
+    match compact_size[0] {
+        0..=252 => compact_size[0] as u64,
+        253 => {
+            let mut buffer = [0; 2];
             transaction_bytes.read(&mut buffer).unwrap();
             u16::from_le_bytes(buffer) as u64
-        },
-        254=>{
-            let mut buffer = [0;4];
+        }
+        254 => {
+            let mut buffer = [0; 4];
             transaction_bytes.read(&mut buffer).unwrap();
             u32::from_le_bytes(buffer) as u64
-        },
-        255=>{
-            let mut buffer = [0;8];
+        }
+        255 => {
+            let mut buffer = [0; 8];
             transaction_bytes.read(&mut buffer).unwrap();
             u64::from_le_bytes(buffer)
         }
@@ -32,20 +34,19 @@ fn read_compact_size(transaction_bytes: &mut &[u8])->u64{
 }
 
 #[allow(unused_variables)]
-fn read_32(transaction_bytes:&mut &[u8])->u32{
-    
-    let mut buffer  = [0; 4];
+fn read_32(transaction_bytes: &mut &[u8]) -> u32 {
+    let mut buffer = [0; 4];
     transaction_bytes.read(&mut buffer).unwrap();
     u32::from_le_bytes(buffer)
 }
 
-fn read_txid(transaction_bytes: &mut &[u8])->[u8; 32]{
+fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
     let mut buffer = [0; 32];
     transaction_bytes.read(&mut buffer).unwrap();
     buffer
 }
 
-fn read_script(transaction_bytes: &mut &[u8])->Vec<u8>{
+fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
     let script_length = read_compact_size(transaction_bytes) as usize;
     let mut buffer = vec![0_u8; script_length];
     transaction_bytes.read(&mut buffer).unwrap();
@@ -53,30 +54,30 @@ fn read_script(transaction_bytes: &mut &[u8])->Vec<u8>{
 }
 
 fn main() {
-    let transaction_hex="010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
+    let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
     let version = read_32(&mut bytes_slice);
     let input_count = read_compact_size(&mut bytes_slice);
     let mut inputs = vec![];
 
-    for _ in 0..input_count{
+    for _ in 0..input_count {
         let txid = read_txid(&mut bytes_slice);
         let output_index = read_32(&mut bytes_slice);
         let script = read_script(&mut bytes_slice);
         let sequence = read_32(&mut bytes_slice);
 
-        inputs.push(Input{
+        inputs.push(Input {
             txid,
             output_index,
             script,
-            sequence
+            sequence,
         });
     }
 
     println!("version: {}!", version);
     println!("input count: {}!", input_count);
-    for input in inputs{
+    for input in inputs {
         println!("txid: {:?}", input.txid);
         println!("output index: {}", input.output_index);
         println!("script: {:?}", input.script);
@@ -85,26 +86,24 @@ fn main() {
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
     #[test]
-    fn test_read_compact_size(){
+    fn test_read_compact_size() {
         let mut bytes_slice = [1_u8].as_slice();
         let compact_size = read_compact_size(&mut bytes_slice);
         assert_eq!(compact_size, 1_u64);
 
-
         let mut bytes_slice = [253_u8, 0, 1].as_slice();
         let compact_size = read_compact_size(&mut bytes_slice);
-        assert_eq!(compact_size,256_u64);
+        assert_eq!(compact_size, 256_u64);
 
         let hex = "fd204e";
         let decode = hex::decode(hex).unwrap();
         let mut bytes_slice = decode.as_slice();
         let compact_size = read_compact_size(&mut bytes_slice);
         let expected_count = 20_000_u64;
-        assert_eq!(compact_size,expected_count);
+        assert_eq!(compact_size, expected_count);
     }
-    
 }
