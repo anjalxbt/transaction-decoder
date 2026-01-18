68e77de6f7aa02a6eac8c4882b615cbd8dace3bf
anjalxbt <anjalehackchey101@gmail.com>
2026-01-18 11:11:11 +0530

    feat: fn to_btc

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,17 @@ struct Input {
     sequence: u32,
 }
 
+struct Amount(u64);
+
+impl Amount {
+    pub fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
 #[derive(Debug, Serialize)]
 struct Output {
-    amount: u64,
+    amount: f64,
     script_pubkey: String,
 }
 
@@ -53,10 +61,10 @@ fn read_32(transaction_bytes: &mut &[u8]) -> u32 {
     u32::from_le_bytes(buffer)
 }
 
-fn read_64(transaction_bytes: &mut &[u8]) -> u64 {
+fn read_amount(transaction_bytes: &mut &[u8]) -> Amount {
     let mut buffer = [0; 8];
     transaction_bytes.read(&mut buffer).unwrap();
-    u64::from_le_bytes(buffer)
+    Amount(u64::from_le_bytes(buffer))
 }
 
 fn read_txid(transaction_bytes: &mut &[u8]) -> String {
@@ -98,7 +106,7 @@ fn main() {
     let mut outputs = vec![];
 
     for _ in 0..output_count {
-        let amount = read_64(&mut bytes_slice) / 100_000_000;
+        let amount = read_amount(&mut bytes_slice).to_btc();
         let script_pubkey = read_script(&mut bytes_slice);
 
         outputs.push(Output {
